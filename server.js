326d49a9c7b37107edc8410abb007c5782b13b72
Ed <edmundluong@gmail.com>
2019-02-20 21:16:05 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser') //middleware: to read input
+const port = 3000;
+
+
+//application settings
+app.set('views','./views')
+app.set('view engine','pug')
+
+//middleware 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public')); //need express to allow access to this folder
+
+//basic route
+app.get('/',(req,res)=>{
+    res.render('index')
+});
+
+app.listen( port,()=>{
+    console.log(`Server is listening on port ${port}`);
+})
